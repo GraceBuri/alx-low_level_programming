4f9282c70838eb48b0fc2c39a18bd78ce69ff09d
GraceBuri <grcbuuri@gmail.com>
2023-02-17 12:43:00 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,18 +1,23 @@
-# include <stdio.h>
+#include <stdio.h>
 /**
- * main - prints all combinations of single digit numbers
- * Return: 0 indicates success
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
-{
-	int c;
-	for (c = 48; c < 57; c++)
-		putchar(c);
-		if ( c==57 )
-			continue;
+	{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+		if ( a < 9)
+		{
+
 		putchar(',');
 		putchar('.');
-
+		}
+	}
 	putchar('\n');
 	return (0);
-}
+	}
