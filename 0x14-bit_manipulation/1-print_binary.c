2441c7d42c5a9aac1712ff868de0f20f469bb30d
GraceBuri <grcbuuri@gmail.com>
2023-04-28 04:30:18 -0700

    betty style corrections

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,39 +1,31 @@
 #include "main.h"
 
 /**
- * _pow - calculates (base ^ power)
- * @base: base to the exponent
- * @power: power to the exponent
- * Return: value of (base ^ power)
- */
-unsigned long int _pow(unsigned int base, unsigned int power)
-{
-	unsigned int i;
-	unsigned long int result = 1;
-
-	for (i = 0; i < power; i++) {
-		result *= base;
-	}
-	return result;
-}
-
-/**
- * print_binary - prints number in binary
- * @n: number to print
- * Return: void
- */
+* print_binary - Prints a binary number without using % or /
+* @n: The number to be printed
+*
+* Return: Nothing
+*/
 void print_binary(unsigned long int n)
 {
-	unsigned long int divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
-	int flag = 0;
+unsigned int flag = 0, max = 32768; /* 1000 0000 0000 0000 */
 
-	while (divisor != 0) {
-		if ((n & divisor) == divisor) {
-			flag = 1;
-			_putchar('1');
-		} else if (flag == 1 || divisor == 1) {
-			_putchar('0');
-		}
-		divisor >>= 1;
-	}
+if (n == 0)
+{
+_putchar('0');
+return;
+}
+
+while (max)
+{
+if (flag == 1 && (n & max) == 0)
+_putchar('0');
+else if ((n & max) != 0)
+{
+_putchar('1');
+flag = 1;
+}
+
+max >>= 1;
+}
 }
