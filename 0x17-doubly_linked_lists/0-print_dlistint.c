029779eba2b9286ee0804cf09a7428d85c3459ea
GraceBuri <grcbuuri@gmail.com>
2023-06-15 19:37:52 -0700

    Add doubly linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,12 +1,9 @@
 #include "lists.h"
 
 /**
-
-print_dlistint - Prints all the elements of a dlistint_t list
-
-@h: Head of the list
-
-Return: The number of nodes
+ * print_dlistint - Prints all the elements of a dlistint_t list
+ * @h: Head of the list
+ * Return: The number of nodes
 */
 size_t print_dlistint(const dlistint_t *h)
 {
@@ -20,5 +17,5 @@ count++;
 current = current->next;
 }
 
-return count;
+return (count);
 }
