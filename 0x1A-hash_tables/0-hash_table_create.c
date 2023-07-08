920db25d1fd4568805ba3517ec3a11ecd6a29401
GraceBuri <grcbuuri@gmail.com>
2023-07-07 17:49:57 -0700

    0x1A-hash_tables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table with a given size
+ *
+ * @size: size of the hash table
+ * Return: the created hash table, or NULL if function fails
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+
+	table->array = calloc(size, sizeof(hash_node_t *));
+	if (table->array == NULL) {
+		free(table);
+		return (NULL);
+	}
+
+	table->size = size;
+
+	return (table);
+}
